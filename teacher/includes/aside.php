1cb838c9b0950af4a7f4c5cc1294241bd34307c9
aditical <alishaacharya77@gmail.com>
2018-07-11 19:11:34 +0545

    Work on Teacher Module

---
+++ teacher/includes/aside.php
@@ -0,0 +1,95 @@
+<aside class="bg-dark lter aside-md hidden-print" id="nav">          
+          <section class="vbox">            
+            <section class="w-f scrollable">
+              <div class="slim-scroll" data-height="auto" data-disable-fade-out="true" data-distance="0" data-size="5px" data-color="#333333">
+                
+                <!-- nav -->
+                <nav class="nav-primary hidden-xs">
+                  <ul class="nav">
+                    <li  class="active">
+                      <a href="teacher.html"   class="active">
+                        <i class="fa fa-dashboard icon">
+                          <b class="bg-danger"></b>
+                        </i>
+                        <span>Dashboard</span>
+                      </a>
+                    </li>
+                    <li >
+                      <a href= "profile.html"  >
+                        <i class="fa fa-user icon">
+                          <b class="bg-warning"></b>
+                        </i>                       
+                        <span>Profile</span>                       
+                      </a>                    
+                    </li>
+                    <li >
+                      <a href= "news.html"  >
+                        <i class="fa fa-list-alt icon">
+                          <b class="bg-info"></b>
+                        </i>                       
+                        <span>News</span>                       
+                      </a>                    
+                    </li>
+                    <li >
+                      <a href="notes.html"  >
+                        <i class="fa fa-pencil-square icon">
+                          <b class="bg-success"></b>
+                        </i>                       
+                        <span>Notes</span>
+                      </a>                     
+                    </li>
+                    <li >
+                      <a href="classes.html"  >
+                        <i class="fa fa-book icon">
+                          <b class="bg-warning"></b>
+                        </i>                      
+                        <span>Classes</span>
+                      </a>                      
+                    </li>                    
+                    <li >
+                      <a href="queries.html"  >
+                        <b class="badge bg-danger pull-right"></b>
+                        <i class="fa fa-question-circle icon">
+                          <b class="bg-primary dker"></b>
+                        </i>
+                        <span>Queries</span>
+                      </a>
+                    </li>
+                    <li >
+                      <a href="notices.html"  >
+                        <i class="fa fa-files-o icon">
+                          <b class="bg-info"></b>
+                        </i>
+                        <span>Notices</span>
+                      </a>
+                    </li>
+                    <li >
+                        <a href="notifications.html"  >
+                          <i class="fa fa-bell icon">
+                            <b class="bg-danger"></b>
+                          </i>
+                          <span>Notifications</span>
+                        </a>
+                      </li>
+                      <li >
+                          <a href="assignments.html"  >
+                            <i class="fa fa-pencil icon">
+                              <b class="bg-primary"></b>
+                            </i>
+                            <span>Assignments</span>
+                          </a>
+                        </li>                        
+                  </ul>
+                </nav>
+                <!-- / nav -->
+              </div>
+            </section>
+            
+            <footer class="footer lt hidden-xs b-t b-dark">        
+               <a href="#nav" data-toggle="class:nav-xs" class="pull-right btn btn-sm btn-dark btn-icon">
+                <i class="fa fa-angle-left text"></i>
+                <i class="fa fa-angle-right text-active"></i>
+              </a>             
+            </footer>
+          </section>
+        </aside>
